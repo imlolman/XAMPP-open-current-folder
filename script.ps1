3f99606e59d984c125a845b272b173d1e9f8fd4c
imsatyabc <satyam.guta756@gmail.com>
2020-08-28 10:26:53 +0530

    Working Script

--- script.ps1
+++ script.ps1
@@ -1,4 +1,14 @@
+$path = $pwd.Path
+
 $file = 'httpd.conf'
-$regex = '(?<=<setting name="Media\.MediaLinkServerUrl" value=")[^"]*'
-(Get-Content $file) -replace $regex, 'https://newurl.com' | Set-Content $file
-pause
+$regex = '(?<=DocumentRoot ")[^"]*'
+(Get-Content $file | Out-String ).Trim() -replace $regex, 'aZCvBE9yX4sNxvx2v6AKeQ2' | Set-Content $file
+
+$file = 'httpd.conf'
+$regex = '(?<=DocumentRoot "aZCvBE9yX4sNxvx2v6AKeQ2"[\r\n]+<Directory ")[^"]*'
+(Get-Content $file | Out-String ).Trim() -replace $regex, $path | Set-Content $file
+
+$file = 'httpd.conf'
+$regex = '(?<=DocumentRoot ")[^"]*'
+(Get-Content $file) -replace $regex, $path | Set-Content $file
+# pause
